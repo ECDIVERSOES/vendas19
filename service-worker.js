1e2eb1cdbf709940533aed98d97a6ce67d985af4
ECDIVERSOES <esiocosta17@gmail.com>
2025-03-05 11:42:21 -0300

    Atualizando service-worker.js via API

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'karaoke-cache-v16'; // Altere a versão para forçar atualização
+const CACHE_NAME = 'karaoke-cache-v35'; // Altere a versão para forçar atualização
 const ASSETS = [
   './',
   './index.html',
